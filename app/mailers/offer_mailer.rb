b3c0083b5457f10b53a8bde99257b7c3c9c27de0
John Davison <johncdavison@gmail.com>
2015-01-14 08:55:08 -0800

    Debug mailer.

--- app/mailers/offer_mailer.rb
+++ app/mailers/offer_mailer.rb
@@ -1,10 +1,10 @@
 class OfferMailer < ActionMailer::Base
-  default from: "jd@startuplandia.io"
+  default 
   SUBJ = 'Antipattern.io offer for code review'
 
   def notify_of_offer args
     @offer_owner = args[:offer_owner]
     @review_request_owner = args[:review_request_owner]
-    mail to: @review_request_owner.email, cc: @offer_owner, subject: SUBJ
+    mail to: @review_request_owner.email, from: 'jd@startuplandia.io', cc: @offer_owner, subject: SUBJ
   end
 end
