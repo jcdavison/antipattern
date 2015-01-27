b5cb6fd3d8a16597b25ef29fb22f367aae9808a0
John Davison <johncdavison@gmail.com>
2015-01-27 08:38:56 -0800

    set prod mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: ENV['MAILER_URL'] }
+  config.action_mailer.default_url_options = { host: 'http://www.antipattern.io' }
 
   config.action_mailer.smtp_settings = {
     address:              'smtp.mailgun.org',
