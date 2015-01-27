22b0ffaea8ab68c463bb7b5b048fc30d0085594b
John Davison <johncdavison@gmail.com>
2015-01-27 08:28:05 -0800

    Tweak mailer copy.

--- app/mailers/offer_mailer.rb
+++ app/mailers/offer_mailer.rb
@@ -1,11 +1,12 @@
 class OfferMailer < ActionMailer::Base
+
   SUBJ = 'Antipattern.io offer for code review'
   def notify_of_offer args
     @offer_owner = args[:offer_owner]
     @review_request_owner = args[:review_request_owner]
     @review_request_id = args[:review_request_id]
     to = @review_request_owner.email
-    cc = @offer_owner.email
-    mail to: to, from: 'jd@startuplandia.io', cc: cc, subject: SUBJ
+    mail to: to, from: 'jd@startuplandia.io', subject: SUBJ
   end
+
 end
