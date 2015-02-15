45df13316804e5338c3e0fd4daa6aeb6e05a35e3
John Davison <johncdavison@gmail.com>
2015-02-15 10:49:10 -0800

    Basic notifications in place.

--- app/mailers/offer_mailer.rb
+++ app/mailers/offer_mailer.rb
@@ -1,27 +1,55 @@
 class OfferMailer < ActionMailer::Base
+  FROM = 'jd@antipattern.io'
 
-  def notify_of_offer args
-    @offer_owner = args[:offer_owner]
-    @review_request_owner = args[:review_request_owner]
-    @review_request_id = args[:review_request_id]
+  def notify_of_offer recipients
+    @offer_owner = recipients[:offer_owner]
+    @review_request_owner = recipients[:review_request_owner]
+    @review_request_id = recipients[:code_review].id
     to = @review_request_owner.email
-    subject = 'Antipattern.io offer for code review'
-    mail to: to, from: 'jd@antipattern.io', subject: subject
+    subject = 'Antipattern.io Code Review offer'
+    mail to: to, from: FROM, subject: subject
   end
 
   def notify_acceptance recipients
     @offer_owner = recipients[:offer_owner]
     @review_request_owner = recipients[:review_request_owner]
+    @code_review = recipients[:code_review]
     to = [ @offer_owner.email, @review_request_owner.email ]
-    subject = 'Antipattern.io acceptance of Code Review Offer'
-    mail to: to, from: 'jd@antipattern.io', subject: subject
+    subject = 'Antipattern.io Code Review offer acceptance'
+    mail to: to, from: FROM, subject: subject
   end
 
   def notify_rejection recipients
     @offer_owner = recipients[:offer_owner]
     @review_request_owner = recipients[:review_request_owner]
     to = @offer_owner.email
-    subject = 'Antipattern.io code review offer status'
-    mail to: to, from: 'jd@antipattern.io', subject: subject
+    subject = 'Antipattern.io Code Review offer update'
+    mail to: to, from: FROM, subject: subject
+  end
+
+  def notify_delivery recipients
+    @offer_owner = recipients[:offer_owner]
+    @review_request_owner = recipients[:review_request_owner]
+    @code_review = recipients[:code_review]
+    to = [ @offer_owner.email, @review_request_owner.email ]
+    subject = 'Antipattern.io Code Review delivery'
+    mail to: to, from: FROM, subject: subject
+  end
+
+  def notify_pay recipients
+    @offer_owner = recipients[:offer_owner]
+    @review_request_owner = recipients[:review_request_owner]
+    @code_review = recipients[:code_review]
+    to = @offer_owner.email
+    subject = 'Antipattern.io Code Review acceptance and pmt'
+    mail to: to, from: FROM, subject: subject
+  end
+
+  def notify_dispute recipients
+    @offer_owner = recipients[:offer_owner]
+    @review_request_owner = recipients[:review_request_owner]
+    @code_review = recipients[:code_review]
+    @offer = recipients[:offer]
+    mail to: 'jd@startuplandia.io', from: FROM, subject: 'Antipattern dispute'
   end
 end
