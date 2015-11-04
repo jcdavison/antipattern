57044dfe85bec8f7ce80891f2f4115c57594af77
John Davison <johncdavison@gmail.com>
2015-11-03 16:15:39 -0800

    Bump result count.

--- app/controllers/api/repositories_controller.rb
+++ app/controllers/api/repositories_controller.rb
@@ -1,6 +1,6 @@
 class Api::RepositoriesController < ApplicationController
   def index
-    repos = OCTOCLIENT.get("/users/#{current_user.github_username}/repos")
+    repos = OCTOCLIENT.get("/users/#{current_user.github_username}/repos", per_page: 100)
     repo_names = repos.map {|repo, index| {text: repo[:name], id: repo[:name]} }
     render json: { repos: repo_names }
     rescue
